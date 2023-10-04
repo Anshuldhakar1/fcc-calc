e391390d7a48d807943ab290dcee34f1de534256
Anshul <anshuldhakar03@gmail.com>
2023-10-04 19:34:58 +0530

    initial workspace

---
+++ free-code-camp-calc/workspace/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="free-code-camp-calc/workspace" />
+    </div>
+  );
+}
+
+export default App;
